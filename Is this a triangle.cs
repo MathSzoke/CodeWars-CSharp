3c8a0480ecf5d508189eddcddb5274fc3b265d4b
Matheus Henrique Szoke La Motta <84111080+MathSzoke@users.noreply.github.com>
2024-01-04 17:14:34 -0300

    Renaming all files to add the difficulty before the name challenge.

--- Is this a triangle.cs
+++
@@ -1,6 +0,0 @@
-public class Triangle
-{
-	// Check if the sides satisfy the triangle inequality theorem
-	// The sum of the lengths of any two sides must be greater than the length of the third side.
-    public static bool IsTriangle(int a, int b, int c) => (a + b > c) && (b + c > a) && (a + c > b);
-}
